976c623f61d4687d61b25600018fb47cf28e88dc
juhee kang <mnmn092631@gmail.com>
2023-04-10 13:50:38 +0900

    JavaScript | getElementById, addEventListener

---
+++ script/1_3.js
@@ -0,0 +1,10 @@
+const h2 = document.getElementById("h2id");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  h2.innerHTML = "안녕하세요";
+});
+
+// function show() {
+//   alert("안녕하세요");
+// }
