e8fd68f97700207c55784174eb38c1a3579c3142
juhee kang <mnmn092631@gmail.com>
2023-04-11 09:34:15 +0900

    1_3 코드 수정

--- script/1_3.js
+++ script/1_3.js
@@ -1,10 +1,11 @@
 const h2 = document.getElementById("h2id");
 const btn = document.querySelector("button");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
 btn.addEventListener("click", () => {
   h2.innerHTML = "안녕하세요";
 });
-
-// function show() {
-//   alert("안녕하세요");
-// }
