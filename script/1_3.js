051be852f57cc93570ce18068f22ba29d6bb0f34
juhee kang <mnmn092631@gmail.com>
2023-04-11 10:57:57 +0900

    querySelector, preventDefault, DOMContentLoaded

--- script/1_3.js
+++ script/1_3.js
@@ -1,11 +1,19 @@
-const h2 = document.getElementById("h2id");
-const btn = document.querySelector("button");
-const form = document.querySelector("form");
+// document.addEventListener("DOMContentLoaded", () => {});
 
+const h2 = document.querySelector("#h2id.h2Class");
+h2.innerHTML = "아이디 선택자 시작입니다.";
+
+const newBtn = document.createElement("button");
+// newBtn.innerHTML = "새로운 버튼입니다";
+newBtn.textContent = "확인";
+document.getElementById("hdiv").append(newBtn);
+
+const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-btn.addEventListener("click", () => {
+const clickBtn = document.querySelector("form > button");
+clickBtn.addEventListener("click", () => {
   h2.innerHTML = "안녕하세요";
 });
