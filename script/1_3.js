5db2815995bc96a671f2a7108d8b101a035bd01d
juhee kang <mnmn092631@gmail.com>
2023-04-12 10:10:26 +0900

    주사위 게임 수정

--- script/1_3.js
+++ script/1_3.js
@@ -8,20 +8,30 @@ const h2 = document.querySelector("#h2id");
 // newBtn.textContent = "확인";
 // document.getElementById("hdiv").append(newBtn);
 
-// const form = document.querySelector("form");
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-// });
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-const clickBtn = document.querySelector("form > button");
+const clickBtn = document.querySelector("form button[type='submit']");
 const imgDiv = document.querySelector("article > div");
 imgDiv.style.textAlign = "center";
+
 clickBtn.addEventListener("click", () => {
   // h2.innerHTML = "안녕하세요";
+  const checkedInput = document.querySelector("input:checked");
+  if (checkedInput == null) return alert("숫자를 선택해주세요.");
+
   let diceNum = Math.floor(Math.random() * 6) + 1;
   imgDiv.innerHTML = `<img src="./img/${diceNum}.png" alt="dice num${diceNum}" / >`;
 
-  const checkedInput = document.querySelector("input:checked");
   if (diceNum == checkedInput.value) h2.innerHTML = "정답입니다.";
   else h2.innerHTML = "틀렸습니다.";
+  h2.style.textAlign = "center";
+});
+
+const resetBtn = document.querySelector("form button[type='reset']");
+resetBtn.addEventListener("click", () => {
+  imgDiv.innerHTML = "";
+  h2.innerHTML = "";
 });
