fd6cf52e616e67f8c9b2708b515859573d219c74
juhee kang <mnmn092631@gmail.com>
2023-04-13 13:03:40 +0900

    배열 다루기

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [Pages](https://mnmn092631.github.io/K-digital-2023-3-JS/)
 
-- 23.4.13. : 문자열 다루기, slice, split, reverse, join
+- 23.4.13. : 문자열 다루기(slice, split, reverse, join), 배열 다루기(push)
 - 23.4.12. : 주사위게임, DOMContentLoaded, changeEvent, inputEvent
 - 23.4.11. : querySelectorAll, forEach
 - 23.4.10. : HTML - Form, JavaScript - addEventListener, getElementById
---
+++ script/1_8.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const resultTxt = document.querySelector("#resultTxt");
+  let arr = [];
+
+  // 배열에 자료 추가
+  const createBtns = document.querySelectorAll("div.create > button");
+  createBtns.forEach((createBtn) => {
+    createBtn.addEventListener("click", () => {
+      switch (createBtn.innerHTML) {
+        case "사과":
+          arr.push("🍎");
+          break;
+        case "바나나":
+          arr.push("🍌");
+          break;
+        case "오렌지":
+          arr.push("🍊");
+          break;
+        case "수박":
+          arr.push("🍉");
+          break;
+      }
+      resultTxt.value = arr.join("");
+    });
+  });
+
+  // 배열에 자료 삭제
+  const deleteBtns = document.querySelectorAll("div.delete > button");
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", () => {
+      switch (deleteBtn.innerHTML) {
+        case "사과삭제":
+          arr.splice(arr.lastIndexOf("🍎"), 1);
+          break;
+        case "바나나삭제":
+          arr.splice(arr.lastIndexOf("🍌"), 1);
+          break;
+        case "오렌지삭제":
+          arr.splice(arr.lastIndexOf("🍊"), 1);
+          break;
+        case "수박삭제":
+          arr.splice(arr.lastIndexOf("🍉"), 1);
+          break;
+      }
+      resultTxt.value = arr.join("");
+    });
+  });
+
+  // 배열에 자료 변경
+  const updateBtns = document.querySelectorAll("div.update > button");
+  updateBtns.forEach((updateBtn) => {
+    updateBtn.addEventListener("click", () => {
+      switch (updateBtn.innerHTML) {
+        case "사과-&gt;당근":
+          arr = arr.map((item) => {
+            return item === "🍎" ? (item = "🥕") : item;
+          });
+          break;
+        case "바나나-&gt;오이":
+          arr = arr.map((item) => {
+            return item === "🍌" ? (item = "🥒") : item;
+          });
+          break;
+        case "오렌지-&gt;아보카도":
+          arr = arr.map((item) => {
+            return item === "🍊" ? (item = "🥑") : item;
+          });
+          break;
+        case "수박-&gt;브로콜리":
+          arr = arr.map((item) => {
+            return item === "🍉" ? (item = "🥦") : item;
+          });
+          break;
+      }
+      resultTxt.value = arr.join("");
+    });
+  });
+
+  // reset 버튼
+  const resetBtn = document.querySelector("#reset");
+  resetBtn.addEventListener("click", () => {
+    resultTxt.value = "";
+    arr = [];
+  });
+});
