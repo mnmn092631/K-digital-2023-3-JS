ddd558d22271f9fab12061216dc0d0ba7808a232
juhee kang <mnmn092631@gmail.com>
2023-04-14 12:23:56 +0900

    배열 다루기 수정

--- script/1_8.js
+++ script/1_8.js
@@ -1,25 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const resultTxt = document.querySelector("#resultTxt");
+  const fruit = {
+    사과: "🍎",
+    바나나: "🍌",
+    오렌지: "🍊",
+    수박: "🍉",
+    당근: "🥕",
+    오이: "🥒",
+    아보카도: "🥑",
+    브로콜리: "🥦",
+  };
   let arr = [];
 
   // 배열에 자료 추가
   const createBtns = document.querySelectorAll("div.create > button");
   createBtns.forEach((createBtn) => {
     createBtn.addEventListener("click", () => {
-      switch (createBtn.innerHTML) {
-        case "사과":
-          arr.push("🍎");
-          break;
-        case "바나나":
-          arr.push("🍌");
-          break;
-        case "오렌지":
-          arr.push("🍊");
-          break;
-        case "수박":
-          arr.push("🍉");
-          break;
-      }
+      arr.push(fruit[createBtn.innerHTML]);
       resultTxt.value = arr.join("");
     });
   });
@@ -28,24 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const deleteBtns = document.querySelectorAll("div.delete > button");
   deleteBtns.forEach((deleteBtn) => {
     deleteBtn.addEventListener("click", () => {
-      switch (deleteBtn.innerHTML) {
-        case "사과삭제":
-          if (arr.lastIndexOf("🍎") === -1) break;
-          arr.splice(arr.lastIndexOf("🍎"), 1);
-          break;
-        case "바나나삭제":
-          if (arr.lastIndexOf("🍌") === -1) break;
-          arr.splice(arr.lastIndexOf("🍌"), 1);
-          break;
-        case "오렌지삭제":
-          if (arr.lastIndexOf("🍊") === -1) break;
-          arr.splice(arr.lastIndexOf("🍊"), 1);
-          break;
-        case "수박삭제":
-          if (arr.lastIndexOf("🍉") === -1) break;
-          arr.splice(arr.lastIndexOf("🍉"), 1);
-          break;
-      }
+      let clickedValue = deleteBtn.innerHTML.replace("삭제", "");
+      if (arr.lastIndexOf(fruit[clickedValue]) === -1) return;
+      arr.splice(arr.lastIndexOf(fruit[clickedValue]), 1);
       resultTxt.value = arr.join("");
     });
   });
@@ -53,28 +35,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const deleteAllBtns = document.querySelectorAll("div.deleteAll > button");
   deleteAllBtns.forEach((deleteAllBtn) => {
     deleteAllBtn.addEventListener("click", () => {
-      switch (deleteAllBtn.innerHTML) {
-        case "사과전체삭제":
-          arr = arr.filter((item) => {
-            return item !== "🍎";
-          });
-          break;
-        case "바나나전체삭제":
-          arr = arr.filter((item) => {
-            return item !== "🍌";
-          });
-          break;
-        case "오렌지전체삭제":
-          arr = arr.filter((item) => {
-            return item !== "🍊";
-          });
-          break;
-        case "수박전체삭제":
-          arr = arr.filter((item) => {
-            return item !== "🍉";
-          });
-          break;
-      }
+      let clickedValue = deleteAllBtn.innerHTML.replace("전체삭제", "");
+      arr = arr.filter((item) => {
+        return item !== fruit[clickedValue];
+      });
       resultTxt.value = arr.join("");
     });
   });
@@ -83,28 +47,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const updateBtns = document.querySelectorAll("div.update > button");
   updateBtns.forEach((updateBtn) => {
     updateBtn.addEventListener("click", () => {
-      switch (updateBtn.innerHTML) {
-        case "사과-&gt;당근":
-          arr = arr.map((item) => {
-            return item === "🍎" ? (item = "🥕") : item;
-          });
-          break;
-        case "바나나-&gt;오이":
-          arr = arr.map((item) => {
-            return item === "🍌" ? (item = "🥒") : item;
-          });
-          break;
-        case "오렌지-&gt;아보카도":
-          arr = arr.map((item) => {
-            return item === "🍊" ? (item = "🥑") : item;
-          });
-          break;
-        case "수박-&gt;브로콜리":
-          arr = arr.map((item) => {
-            return item === "🍉" ? (item = "🥦") : item;
-          });
-          break;
-      }
+      let fromValue = updateBtn.innerHTML.split("-&gt;")[0];
+      let toValue = updateBtn.innerHTML.split("-&gt;")[1];
+      arr = arr.map((item) => {
+        return item === fruit[fromValue] ? fruit[toValue] : item;
+      });
       resultTxt.value = arr.join("");
     });
   });
